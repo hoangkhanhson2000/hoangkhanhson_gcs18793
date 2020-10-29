b80af0fe841f617dbd184892a4043b945be5614c
hoangkhanhson2000 <66456579+hoangkhanhson2000@users.noreply.github.com>
2020-10-29 20:05:35 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -8,30 +8,23 @@ session_start();
 </head>
 <body>
 <?php
-	//Gọi file connection.php ở bài trước
+	
 	require_once("config.php");
-	// Kiểm tra nếu người dùng đã ân nút đăng nhập thì mới xử lý
 	if (isset($_POST["btn_submit"])) {
-		// lấy thông tin người dùng
+	
 		$username = $_POST["username"];
 		$password = $_POST["passwordd"];
-		//làm sạch thông tin, xóa bỏ các tag html, ký tự đặc biệt 
-		//mà người dùng cố tình thêm vào để tấn công theo phương thức sql injection
-	
-		if ($username == "" || $passwordd =="") {
-			echo "username hoặc password bạn không được để trống!";
-		}else{
+	   $db =getDB();
+	   $msg = '';
+		if ($db) {
 			$query = "SELECT * FROM Account WHERE username = $username";
 			$result = pg_query($query);
-			if ($result==0) {
-				echo "tên đăng nhập hoặc mật khẩu không đúng !";
-			}else{
-				//tiến hành lưu tên đăng nhập vào session để tiện xử lý sau này
-				$_SESSION['username'] = $username;
-                // Thực thi hành động sau khi lưu thông tin vào session
-                // ở đây mình tiến hành chuyển hướng trang web tới một trang gọi là index.php
-                header('Location: index.php');
+			if ($result) {
+				$db_password =pg_result ($result, 0, "password");
+			if ($db_passwordd ==$passwordd) {
+					$msg = "Login sucess";
 			}
+ 		}
 		}
 	}
 ?>
@@ -51,9 +44,7 @@ session_start();
 	    			<td colspan="2" align="center"> <input name="btn_submit" type="submit" value="Đăng nhập"></td>
 	    		</tr>
 	    	</table>
-<p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
   </fieldset>
-
   </form>
 </body>
 </html>
